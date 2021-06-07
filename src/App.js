6d76a9eff9da863ec92aeb434b2d027eb6d41964
MaryamPayenda <maryampayanda12@gmail.com>
2021-06-07 11:35:57 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import MorseGenerator from "./components/MorseGenerator";
 
 function App() {
+  const [userInput, setUserInput] = useState("");
+  function changeHandel(e) {
+    setUserInput(e.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="general">
+      <h2>Morse Generator</h2>
+      <div className="container">
+        <div className="input-container">
+          <h3>Enter your text here</h3>
+          <textarea
+            name=""
+            id=""
+            cols="30"
+            rows="10"
+            placeholder="Enter your Text"
+            value={userInput}
+            onChange={changeHandel}
+          ></textarea>
+        </div>
+        <div className="morse-container">
+          <h3> Your Morse code:</h3>
+          <div className="results">
+            <MorseGenerator userInput={userInput} />
+          </div>
+        </div>
+      </div>
+      <p>This Morse app accepts only </p>
+      <ul>
+        <li>Character</li>
+        <li>Numbers</li>
+        <li>Special Symboles</li>
+      </ul>
     </div>
   );
 }
